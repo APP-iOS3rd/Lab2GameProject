e52f6db4c11007f3692d55fdbdc79f692dd7fa95
NohJooYoung <nohjoo97@naver.com>
2023-11-09 16:44:22 +0900

    [add] 라이브러리 화면 구성

--- GameProject/GameProject/LibraryView.swift
+++ GameProject/GameProject/LibraryView.swift
@@ -23,47 +23,90 @@ struct LibraryView: View {
             .padding(.horizontal, 20)
             .padding(.top, 20)
             
-            HStack {
-                Text("RPG")
-                    .font(.title)
-                    .bold()
-                Spacer()
-            }
-            .padding(.horizontal, 20)
-            .padding(.top, 20)
-            
             ScrollView(.vertical) {
-                LazyVGrid(columns: layouts, spacing: 20) {
-                    ForEach($gameObservable.rpg, id: \.self) { (game: Binding<Game>) in
-                        if !game.wrappedValue.isLike {
-                            VStack {
-                                ZStack {
-                                    Image(game.wrappedValue.image)
-                                        .resizable()
-                                        .frame(width: 150, height: 100)
-                                        .aspectRatio(contentMode: .fit)
-                                        
-                                    Button(action: {
-                                        if let index = gameObservable.rpg.firstIndex(of: game.wrappedValue){
-                                            gameObservable.rpg[index].isLike = false
-                                        }
-                                    }, label: {
-                                        Image(systemName: "heart.fill")
-                                            .font(.system(size: 30))
-                                            .foregroundStyle(.pink)
-                                            .padding(.leading, 110)
-                                            .padding(.top, 65)
-                                    })
+                Section(content: {
+                    LazyVGrid(columns: layouts, spacing: 20) {
+                        ForEach($gameObservable.rpg, id: \.self) { (game: Binding<Game>) in
+                            if game.wrappedValue.isLike {
+                                VStack {
+                                    ZStack {
+                                        Image(game.wrappedValue.image)
+                                            .resizable()
+                                            .frame(width: 150, height: 100)
+                                            .aspectRatio(contentMode: .fit)
+                                            
+                                        Button(action: {
+                                            if let index = gameObservable.rpg.firstIndex(of: game.wrappedValue){
+                                                gameObservable.rpg[index].isLike = false
+                                            }
+                                        }, label: {
+                                            Image(systemName: "heart.fill")
+                                                .font(.system(size: 30))
+                                                .foregroundStyle(.pink)
+                                                .padding(.leading, 110)
+                                                .padding(.top, 65)
+                                        })
+                                    }
+                                    Text(game.wrappedValue.name)
+                                        .bold()
                                 }
-                                Text(game.wrappedValue.name)
-                                    .bold()
                             }
                         }
                     }
-                }
-                .padding()
+                    .padding()
+                }, header: {
+                    HStack {
+                        Text("RPG")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                })
+                
+                Section(content: {
+                    LazyVGrid(columns: layouts, spacing: 20) {
+                        ForEach($gameObservable.fps, id: \.self) { (game: Binding<Game>) in
+                            if game.wrappedValue.isLike {
+                                VStack {
+                                    ZStack {
+                                        Image(game.wrappedValue.image)
+                                            .resizable()
+                                            .frame(width: 150, height: 100)
+                                            .aspectRatio(contentMode: .fit)
+                                            
+                                        Button(action: {
+                                            if let index = gameObservable.fps.firstIndex(of: game.wrappedValue){
+                                                gameObservable.fps[index].isLike = false
+                                            }
+                                        }, label: {
+                                            Image(systemName: "heart.fill")
+                                                .font(.system(size: 30))
+                                                .foregroundStyle(.pink)
+                                                .padding(.leading, 110)
+                                                .padding(.top, 65)
+                                        })
+                                    }
+                                    Text(game.wrappedValue.name)
+                                        .bold()
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                }, header: {
+                    HStack {
+                        Text("FPS")
+                            .font(.largeTitle)
+                            .bold()
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 20)
+                })
+                
             }
-            
         }
     }
 }
