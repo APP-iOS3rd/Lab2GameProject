62e4eb564076b8f2fe7d6edb49a5888103e48a42
NohJooYoung <nohjoo97@naver.com>
2023-11-09 15:42:26 +0900

    [add]임시 라이브러리 화면 구성

--- GameProject/GameProject/LibraryView.swift
+++ GameProject/GameProject/LibraryView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct LibraryView: View {
+    @StateObject var gameObservable: GameObservable
+    @State var gameasd: [Game] = [Game(name: "롤", image: "applelogo", category: "RPG", isLike: true), Game(name: "배그", image: "swift", category: "FPS", isLike: false), Game(name: "배그", image: "swift", category: "FPS", isLike: false), Game(name: "배그", image: "swift", category: "FPS", isLike: false), Game(name: "롤", image: "applelogo", category: "RPG", isLike: true)]
+    
+    private let layouts: [GridItem] = [GridItem(), GridItem()]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: layouts, spacing: 5) {
+                    ForEach(gameasd, id: \.self) { index in
+                        if index.isLike {
+                            VStack {
+                                ZStack {
+                                    Image(systemName: index.image)
+                                        .font(.system(size: 100))
+                                    
+                                    Button(action: {
+                                        if let asd = gameasd.firstIndex(of: index){
+                                            gameasd[asd].isLike = false
+                                        }
+                                    }, label: {
+                                        Image(systemName: "heart.fill")
+                                            .foregroundStyle(.pink)
+                                            .padding(.leading, 50)
+                                            .padding(.top, 70)
+                                    })
+                                }
+                                Text(index.name)
+                                    .font(.largeTitle)
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            
+        }
     }
 }
 
 #Preview {
-    LibraryView()
+    LibraryView(gameObservable: GameObservable())
 }
